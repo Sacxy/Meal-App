06cb3a78407f52e0a31ebcc1b762918cb37525e2
Saksham Srivastava <Saksham243@gmail.com>
2020-06-02 01:59:20 +0530

    Category Dummy Data created .

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return GridView(
+      children: <Widget>[],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
     );
   }
 }

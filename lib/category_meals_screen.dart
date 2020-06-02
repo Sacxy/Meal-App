99e169854137779dc28cd7c2978627055ea602f7
Saksham Srivastava <Saksham243@gmail.com>
2020-06-02 09:48:13 +0530

    Adding Routes to Category Meals Screen

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+
+
+  // CategoryMealsScreen(this.categoryId,this.categoryTitle);
+
+
   @override
   Widget build(BuildContext context) {
+  final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,String> ;
+
+  final String categoryId = routeArgs['id'];
+  final String categoryTitle = routeArgs['title'];
     return Scaffold(
         appBar: AppBar(
-          title: Text("The Recipes"),
+          title: Text(categoryTitle),
         ),
         body: Center(
-          child: Text("New Screen"),
+          child: Text(categoryTitle),
         ));
   }
 }

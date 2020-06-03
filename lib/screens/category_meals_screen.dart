ec3ed6c9eb3504696685ccf9062da35436e78967
Saksham Srivastava <Saksham243@gmail.com>
2020-06-04 01:15:16 +0530

    Meal Item Displayed Accordingly

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
 
 import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-
   static const routeName = '/category-meals';
 
   // CategoryMealsScreen(this.categoryId,this.categoryTitle);
 
-
   @override
   Widget build(BuildContext context) {
-  final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,String> ;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
 
-  final String categoryId = routeArgs['id'];
-  final String categoryTitle = routeArgs['title'];
-  final categoryMeals = DUMMY_MEALS.where((meal) {
-    return meal.categories.contains(categoryId);
-  }).toList();
+    final String categoryId = routeArgs['id'];
+    final String categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(categoryTitle),
-        ),
-        body: ListView.builder(itemBuilder: (ctx,index) {
-          
-          return Text(categoryMeals[index].title); 
-
-        },itemCount: categoryMeals.length,),);
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+            duration: categoryMeals[index].duration,
+            imageUrl: categoryMeals[index].imageUrl,
+            title: categoryMeals[index].title,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,10 +1,132 @@
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
+
 class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  MealItem({
+    @required this.affordability,
+    @required this.duration,
+    @required this.imageUrl,
+    @required this.title,
+    @required this.complexity,
+  });
+
+  void selectMeal() {}
+
+  String get complexityItem {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityItem {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Luxurious:
+        return 'Expensive';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.schedule),
+                        SizedBox(width: 6),
+                        Text('$duration'),
+                      ],
+                    ),
+                    // SizedBox(width: 80),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.attach_money),
+                        SizedBox(width: 6),
+                        Text(affordabilityItem),
+                      ],
+                    ),
+                    // SizedBox(width: 50),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.work),
+                        SizedBox(width: 6),
+                        Text(complexityItem),
+                      ],
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
